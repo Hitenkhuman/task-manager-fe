94c9d2c49009d0b9cca9a5cd5d83476fcd9d40b1
hiten-khuman <hiten1512tata@gmail.com>
2024-07-21 17:34:07 +0530

    fix:create task refresh issue

--- src/pages/home/CreateTaskButton.tsx
+++ src/pages/home/CreateTaskButton.tsx
@@ -3,16 +3,20 @@ import { TaskFormOutput, TaskFormModal } from "./TaskFormModal";
 import { Button } from "../../components/buttons/Button";
 import { useCreateTaskMutation } from "../../queries/useCreateTaskMutation";
 import { toast } from "react-toastify";
+import keys from "../../queries/keys";
+import { useQueryClient } from "react-query";
 
 export const CreateTaskButton: React.FC = () => {
   const [showModal, setShowModal] = React.useState(false);
   const createTaskMutation = useCreateTaskMutation();
+  const queryClient = useQueryClient()
 
   const onSubmit = (data: TaskFormOutput) => {
     createTaskMutation.mutate(data, {
       onSuccess: () => {
         setShowModal(false);
         toast.success("Task created successfully");
+        queryClient.invalidateQueries(keys.tasks)
       },
       onError: (error: any) => {
         if (error?.response?.data?.message) {
